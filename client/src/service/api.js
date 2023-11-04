5ef9e95c7676e61fc3d3d06061e40baf9da06f13
Avishka <avishkajangid0709@gmail.com>
2023-11-05 01:18:46 +0530

    first commit

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // const usersUrl = 'http://localhost:3003/users';
-const usersUrl = 'http://localhost:8000';
+const usersUrl = 'https://crud-app-api-4s7y.onrender.com';
 
 export const getUsers = async (id) => {
     id = id || '';
