d14efb55699dc1f40bf4554b68d30e627bfdb25f
Avishka <avishkajangid0709@gmail.com>
2023-11-05 15:19:39 +0530

    add backend url of vercel

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // const usersUrl = 'http://localhost:3003/users';
-const usersUrl = 'http://localhost:8000';
+const usersUrl = 'https://crudapp-api.vercel.app';
 
 export const getUsers = async (id) => {
     id = id || '';
